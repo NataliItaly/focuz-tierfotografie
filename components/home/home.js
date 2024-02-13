83e0e2c13f93be02783314c87f90189e5a5d4d8f
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2024-02-13 09:31:55 +0100

    feat: update slider

--- components/home/home.js
+++ components/home/home.js
@@ -5,7 +5,7 @@ let count = 0;
 function setHomeBg() {
   setInterval(function () {
     count++;
-    count = count < 13 ? count : 1;
+    count = count < 47 ? count : 1;
     homeSection.style.backgroundImage = `url(./images/slider/${count}.jpg)`;
   }, 3000);
 }
